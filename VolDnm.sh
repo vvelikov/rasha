ce5713534d060a3479e83f89ef810dbaf8b58f20
pia <pia@rage-star.org>
2018-04-18 19:33:17 +0200

    latest

--- VolDnm.sh
+++ VolDnm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#echo " *** *** *** VOL DOWN *** *** *** "
+echo " *** *** *** VOL DOWN *** *** *** "
 echo -n "-" >/tmp/omfifom
--- VolUpp.sh
+++ VolUpp.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo " *** *** *** VOL UP *** *** *** "
 echo -n "+" >/tmp/omfifop
