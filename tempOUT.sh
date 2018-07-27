dcac33dc2dce248ccb8bff30fd55316a9bbdbfeb
pia <pia@rage-star.org>
2018-07-27 16:24:22 +0200

    latest

--- tempOUT.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-URL='http://www.wetter.com/deutschland/muenchen_stadt/wetterstation/DEXXX0266.html'
-
-wget -q -O- "$URL" | grep "text--white" | grep palm-hide | awk -F" " '{print $4}' | cut -d'>' -f2 | cut -d'<' -f1 | sed 's/..$//' > /tmp/temp.out
-wget -q -O- "$URL" | grep "text--small" | grep "</span>" | head -n 14 | tail -n 1| awk -F'"' '{print $5}'| cut -d'<' -f1 | cut -d'>' -f2 > /tmp/weather.txt
--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# temp IN
+python /home/pi/scripts/tempIN.py
+sleep 2
+
+# temp OUT
+URL='http://www.wetter.com/deutschland/muenchen_stadt/wetterstation/DEXXX0266.html'
+
+wget -q -O- "$URL" | grep "text--white" | grep palm-hide | awk -F" " '{print $4}' | cut -d'>' -f2 | cut -d'<' -f1 | sed 's/..$//' > /tmp/temp.out
+wget -q -O- "$URL" | grep "text--small" | grep "</span>" | head -n 14 | tail -n 1| awk -F'"' '{print $5}'| cut -d'<' -f1 | cut -d'>' -f2 > /tmp/weather.txt
+
+sleep 2
+
 tempIN=$(cat /tmp/temp_hum.in | tr [:space:] \\n | head -n 1 | tr -d '\n')
 hum=$(cat /tmp/temp_hum.in | tr [:space:] \\n | tail -n 1)
 tempOUT=$(cat /tmp/temp.out | tr -d '\n')
 rrdtool update /var/www/html/tmp/Temp.rrd -t tempIN:tempOUT:humidity N:$tempIN:$tempOUT:$hum
+
