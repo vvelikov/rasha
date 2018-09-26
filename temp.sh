7073ccd0088153ce69fa0a4e8ccb047004c6e1b8
pia <pia@rage-star.org>
2018-09-26 16:20:56 +0200

    latest

--- temp.sh
+++ temp.sh
@@ -1,11 +1,9 @@
-
 #!/bin/bash
-
 # log
 FILE="/tmp/temp.log"
-# url temp out
-URL1='http://www.wetter.com/deutschland/muenchen_stadt/wetterstation/DEXXX0266.html'
-URL2='http://www.accuweather.com/en/de/munich/80331/weather-forecast/178086'
+TMP="/tmp/temp"
+# url temp out & weather
+URL='http://api.openweathermap.org/data/2.5/weather?q=Munich&APPID=062022b66b18e6ab7dec7f8deb26b1ce&units=metric'
 # dth22
 SCRIPT="/home/pi/scripts/AdafruitDHT.py 22 4"
 # temp & humidity inside
@@ -23,19 +21,22 @@ if [ -f $FILE ]; then
     rm $FILE
     echo $TEMPERATURE > $FILE
     echo $HUMIDITY >> $FILE
-    # temp OUT
-    wget -q -O- "$URL1" | grep "text--white" | grep palm-hide | awk -F" " '{print $4}' | cut -d'>' -f2 | cut -d'<' -f1 | sed 's/..$//' | tr -s , . | sed 's/[^[:print:]\t]//g'   >> $FILE
-    wget -q -O- "$URL2" | grep "cond" | head -n1 | awk -F'">' '{print $2}' | cut -d'<' -f1 >> $FILE
-    # update rrds
-    tempIN=$(cat /tmp/temp.log | head -n 1 | tr -d '\n')
-    hum=$(cat /tmp/temp.log | tail -n 3 | head -n 1 | tr -d '\n')
-    tempOUT=$(cat /tmp/temp.log | tail -n 2 | head -n 1 | tr -d '\n')
-    rrdtool update /var/www/html/tmp/Temp.rrd -t tempIN:tempOUT:humidity N:$tempIN:$tempOUT:$hum
+	# temp OUT
+	wget -q -O- "$URL" > $TMP
+	if [ -s $TMP ]; then
+   	   cat $TMP | awk -F "temp" '{print $2}' | cut -d ":" -f2 | cut -d "," -f1 | xargs printf "%.*f\n" 1 >> $FILE
+	fi
+	if [ -s $TMP ]; then
+	   cat $TMP | awk -F "description" '{print $2}' | cut -d "," -f1 | tr -d '\"' | tr -d ":"  >> $FILE
+	fi
+	# update rrds
+	tempIN=$(cat /tmp/temp.log | head -n 1 | tr -d '\n')
+	hum=$(cat /tmp/temp.log | tail -n 3 | head -n 1 | tr -d '\n')
+	tempOUT=$(cat /tmp/temp.log | tail -n 2 | head -n 1 | tr -d '\n')
+	rrdtool update /var/www/html/tmp/Temp.rrd -t tempIN:tempOUT:humidity N:$tempIN:$tempOUT:$hum
+fi
+if [ -f $TMP ]; then
+	rm $TMP
 fi
 
 exit $?
-
-
-
-
-
